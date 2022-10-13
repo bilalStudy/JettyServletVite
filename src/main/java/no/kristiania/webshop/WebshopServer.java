1be72dd97c09e087b3ab0cbc5f856995833c0041
bilalStudy <biah001@student.kristiania.no>
2022-10-13 18:45:06 +0200

    updating pom, implementing webshop server and adding dependency to tests

---
+++ src/main/java/no/kristiania/webshop/WebshopServer.java
@@ -0,0 +1,14 @@
+package no.kristiania.webshop;
+
+import java.net.URL;
+
+public class WebshopServer {
+
+    public WebshopServer(int port){
+
+    }
+
+    public URL getURL(){
+        return null;
+    }
+}
--- src/test/java/no/kristiania/webshop/WebshopServerTest.java
+++ src/test/java/no/kristiania/webshop/WebshopServerTest.java
@@ -1,4 +1,26 @@
 package no.kristiania.webshop;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class WebshopServerTest {
+
+    @Test
+    void shouldServeHomePage() throws IOException {
+        var server = new WebshopServer(0);
+        var connection = (HttpURLConnection) server.getURL().openConnection();
+
+        assertThat(connection.getResponseCode())
+                .as(connection.getResponseMessage() + " for " + connection.getURL())
+                .isEqualTo(200);
+        assertThat(connection.getInputStream())
+                .asString(StandardCharsets.UTF_8)
+                .contains("<h1>Kristiania Webshop</h1>");
+    }
+
 }
