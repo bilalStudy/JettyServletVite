6cbc170fc34603dfdbc8137f684f7edbfb53c679
bilalStudy <biah001@student.kristiania.no>
2022-10-13 19:00:38 +0200

    now serves context after updating server and fix logging

--- src/main/java/no/kristiania/webshop/WebshopServer.java
+++ src/main/java/no/kristiania/webshop/WebshopServer.java
@@ -3,6 +3,8 @@ package no.kristiania.webshop;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,6 +12,8 @@ import java.net.URL;
 public class WebshopServer {
 
     private final Server server;
+
+    public static final Logger logger = LoggerFactory.getLogger(WebshopServer.class);
 
     public WebshopServer(int port){
         this.server = new Server(port);
@@ -27,5 +31,11 @@ public class WebshopServer {
 
     public void start() throws Exception {
         server.start();
+    }
+
+    public static void main(String[] args) throws Exception {
+        var server = new WebshopServer(8080);
+        server.start();
+        logger.warn("server starting at {}", server.getURL());
     }
 }
