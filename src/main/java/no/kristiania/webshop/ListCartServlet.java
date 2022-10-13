a94318f79315a2d658612ca5ca279ada3bae9838
bilalStudy <biah001@student.kristiania.no>
2022-10-13 19:41:08 +0200

    creating cartitem and updating its servlet

--- src/main/java/no/kristiania/webshop/ListCartServlet.java
+++ src/main/java/no/kristiania/webshop/ListCartServlet.java
@@ -6,10 +6,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ListCartServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        var cartItem = new CartItem();
+        cartItem.setItemName("bread");
+        cartItem.setPrice(100);
+
+        var cartItems = List.of(cartItem);
+
 
         resp.getWriter().println("[{\"title\":\"Java Webshop\"}]");
 
