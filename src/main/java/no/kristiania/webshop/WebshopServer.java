48a36d4ef61f5bbea4f352ea52d604f7fc428e79
bilalStudy <biah001@student.kristiania.no>
2022-10-13 20:18:54 +0200

    changing code for source directory

--- src/main/java/no/kristiania/webshop/WebshopServer.java
+++ src/main/java/no/kristiania/webshop/WebshopServer.java
@@ -1,12 +1,15 @@
 package no.kristiania.webshop;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -16,12 +19,25 @@ public class WebshopServer {
 
     public static final Logger logger = LoggerFactory.getLogger(WebshopServer.class);
 
-    public WebshopServer(int port){
+    public WebshopServer(int port) throws IOException {
         this.server = new Server(port);
 
         WebAppContext webContext = new WebAppContext();
         webContext.setContextPath("/");
-        webContext.setBaseResource(Resource.newClassPathResource("/webapp"));
+
+        Resource resources = Resource.newClassPathResource("/webapp");
+
+        var sourceDirectory = new File(resources.getFile().getAbsoluteFile().toString()
+                .replace('\\', '/')
+                .replace("target/classes", "src/main/resources"));
+        if (sourceDirectory.isDirectory()) {
+            webContext.setBaseResource(Resource.newResource(sourceDirectory));
+            webContext.setInitParameter(DefaultServlet.CONTEXT_INIT + "useFileMappedBuffer", "false");
+        } else {
+            webContext.setBaseResource(resources);
+        }
+
+
         webContext.addServlet(new ServletHolder(new ListCartServlet()),"/api/cart");
         server.setHandler(webContext);
 
